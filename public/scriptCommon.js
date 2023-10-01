d25d3a81d460fc2aed6df37c4689aa21e5b139c9
Shrikant Lahase <138747494+lahaseshrikant@users.noreply.github.com>
2023-10-01 17:17:36 +0530

    Engineer-Mitra 1st uploading to github

---
+++ public/scriptCommon.js
@@ -0,0 +1,9 @@
+function myFunction1() {
+    console.log("Function called");
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
