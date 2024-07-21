103e8de261fe9fd74b275b413a84996e040fca9d
Shrikant Lahase <shrikantlahase143@gmail.com>
2024-07-21 11:24:22 +0530

    ..

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,18 @@
 const functions = require('firebase-functions');
-const express = require('express');
-const rendertron = require('rendertron-middleware');
+const fetch = require('node-fetch');
 
-const app = express();
+const RENDERTRON_URL = 'https://YOUR_PROJECT_ID.appspot.com/render'; // Replace with your Rendertron instance URL
 
-const rendertronMiddleware = rendertron.makeMiddleware({
-  proxyUrl: 'https://render-tron.appspot.com/render' // Using the hosted Rendertron service
+exports.rendertron = functions.https.onRequest((req, res) => {
+  const url = `${RENDERTRON_URL}/${req.url}`;
+  fetch(url)
+    .then(response => response.text())
+    .then(body => {
+      res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+      res.status(200).send(body.toString());
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Error rendering page');
+    });
 });
-
-app.use(rendertronMiddleware);
-app.use(express.static('public'));
-
-exports.rendertron = functions.https.onRequest(app);
-
